aef21e80935dfcd47c63a3f4d47bd2bc0fba79d2
DaniSan0508-project <danielsantoscarneiro0508@gmail.com>
2021-07-18 18:46:09 -0300

    criado migracoes e tabela de produtos no SQLite

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,12 @@
-import * as express from 'express';
+import express from 'express';
+import routes from './routes/index';
 
 const PORT = process.env.PORT || 3333;
 
 const app = express();
+
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333, () => {
   console.log(`servidor rodando na porta ${PORT}`);
